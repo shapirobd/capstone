71d2641b27871e5d6bc87090788ade75068f76be
Brian D. Shapiro <briand.shapiro@Brians-MBP.attlocal.net>
2020-11-02 16:12:42 -0500

    changed seed.py

--- seed.py
+++ seed.py
@@ -1,18 +1,23 @@
 import requests
 
 from app import db
-from models import connect_db, Card
-print('STARTING SEED...')
-db.drop_all()
-db.create_all()
+from models import connect_db, Card, User, Friendship, Deck, Bookmark, Post, CardDeck
 
-for page in range(1, 546):
-    print(f"Page: {page}...")
-    resp = requests.get('http://api.magicthegathering.io/v1/cards', {
-        'key': "$2a$10$TNyqKQQQSzVjgGXY87waZuBIKAS78.NkY2o.H004TfBU.eISv.Pt6",
-        'page': page
-    }).json()
-    cards = resp['cards']
-    Card.create_all_cards(cards)
-    print('** DONE **')
+print('STARTING SEED...')
+
+Post.query.delete()
+Bookmark.query.delete()
+CardDeck.query.delete()
+Deck.query.delete()
+Friendship.query.delete()
+
+# for page in range(1, 546):
+#     print(f"Page: {page}...")
+#     resp = requests.get('http://api.magicthegathering.io/v1/cards', {
+#         'key': "$2a$10$TNyqKQQQSzVjgGXY87waZuBIKAS78.NkY2o.H004TfBU.eISv.Pt6",
+#         'page': page
+#     }).json()
+#     cards = resp['cards']
+#     Card.create_all_cards(cards)
+#     print('** DONE **')
 db.session.commit()
