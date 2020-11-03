b03ff1904bb43854a3ae260621f1c41877c10cef
Brian D. Shapiro <briand.shapiro@Brians-MBP.attlocal.net>
2020-11-03 14:45:40 -0500

    updated seed.py for heroku

--- seed.py
+++ seed.py
@@ -10,14 +10,18 @@ Bookmark.query.delete()
 CardDeck.query.delete()
 Deck.query.delete()
 Friendship.query.delete()
+User.query.delete()
 
-# for page in range(1, 546):
-#     print(f"Page: {page}...")
-#     resp = requests.get('http://api.magicthegathering.io/v1/cards', {
-#         'key': "$2a$10$TNyqKQQQSzVjgGXY87waZuBIKAS78.NkY2o.H004TfBU.eISv.Pt6",
-#         'page': page
-#     }).json()
-#     cards = resp['cards']
-#     Card.create_all_cards(cards)
-#     print('** DONE **')
+db.drop_all()
+db.create_all()
+
+for page in range(1, 546):
+    print(f"Page: {page}...")
+    resp = requests.get('http://api.magicthegathering.io/v1/cards', {
+        'key': "$2a$10$TNyqKQQQSzVjgGXY87waZuBIKAS78.NkY2o.H004TfBU.eISv.Pt6",
+        'page': page
+    }).json()
+    cards = resp['cards']
+    Card.create_all_cards(cards)
+    print('** DONE **')
 db.session.commit()
